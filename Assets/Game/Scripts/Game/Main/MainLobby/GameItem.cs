c267a82d8d8cdc074beaf72db34db9d2fd4fef33
ULynn <xiongsonglin_love@163.com>
2017-08-02 18:25:19 +0800

    main scene

---
+++ Assets/Game/Scripts/Game/Main/MainLobby/GameItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public struct GameItemData
+{
+    public string title;
+    public string name;
+    public string desc;
+    public GameItemData(string title, string name = "", string desc = "")
+    {
+        this.name = name;
+        this.desc = desc;
+        this.title = title;
+    }
+}
+public class GameItem : MonoBehaviour {
+    public GameItemData data;
+    public Image gameImage;
+    public Text gameText;
+
+    private void Awake()
+    {
+        gameImage = GetComponentInChildren<Image>();
+        gameText = GetComponentInChildren<Text>();
+    }
+
+    public void SetGameTex(GameItemData data, UnityEngine.Sprite sp = null)
+    {
+        this.data = data;
+        gameText.text = data.name;
+        if(sp != null)
+            gameImage.sprite = sp;
+    }
+}
--- Assets/Game/Scripts/Game/Main/MainLobby/MainLobbyView.cs
+++ Assets/Game/Scripts/Game/Main/MainLobby/MainLobbyView.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 using SimpleFramework;
 using UnityEngine.UI;
 using SimpleFramework.Game;
+
 public class MainLobbyView : BaseView
 {
+ 
+    public GameItemData[] gameNames = new GameItemData[] { new GameItemData(GameEnum.a,"西游记",""),new GameItemData(GameEnum.b,"斗破苍穹",""),new GameItemData(GameEnum.c,"战狼",""),new GameItemData(GameEnum.d,"天龙八部",""),new GameItemData(GameEnum.e,"金瓶梅",""), new GameItemData(GameEnum.f,"屠龙宝刀",""), new GameItemData(GameEnum.g,"深海捕鱼",""),new GameItemData(GameEnum.h,"丧尸围城","") };
+    protected override void Start()
+    {
+        base.Start();
+        GameItem[] gItems = GetComponentsInChildren<GameItem>();
+        int min = Mathf.Min(gameNames.Length, gItems.Length);
+        for (int i = 0; i < min; i++)
+        {
+            gItems[i].SetGameTex(gameNames[i]);
+        }
+    }
     protected override void OnClicked(Button sender)
     {
         base.OnClicked(sender);
         string name = sender.name;
-        if (name.Contains("game_"))
+        GameItem gi = sender.GetComponent<GameItem>();
+        if (gi != null)
         {
-            IGameBase gb = AppFacade.Ins.GetMgr<GameManager>().GetGame(name);
+            IGameBase gb = AppFacade.Ins.GetMgr<GameManager>().GetGame(gi.data.name);
             if (gb != null) gb.StartUp();
+            Debug.Log("启动游戏："+gi.data.title);
+            return;
         }
-        else if (name.Contains("Setting"))
+        if (name.Contains("Setting"))
         {
 
         }
