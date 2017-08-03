59114efde33ac1f189af74fdeec5dda4571c10af
ULynn <xiongsonglin_love@163.com>
2017-08-03 10:07:36 +0800

    add lauguage module and excel tool

---
+++ Assets/Game/Scripts/Lauguage/LauguageData.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.Net;
+
+class LauguageData
+{
+    private string filepath;
+    private LauguageType _type;
+    public LauguageType curLauguage
+    {
+        get { return _type; }
+    }
+    private Dictionary<string, string> _dic;
+    public LauguageData(string filePath, LauguageType defaultLau = LauguageType.China)
+    {
+        this.filepath = filePath;
+        _type = LauguageType.NULL;
+        _dic = new Dictionary<string, string>();
+        SetLauguageType(defaultLau);
+    }
+    public void SetLauguageType(LauguageType type)
+    {
+        if (_type == type) return;
+        _type = type;
+        if (System.IO.File.Exists(filepath))
+        {
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filepath))
+            {             
+                string str = sr.ReadToEnd();
+                writeToDic(str);
+            }
+        }
+        else
+        {
+            TextAsset ta = Resources.Load<TextAsset>("lauguageConfig");
+            if (ta != null)
+                writeToDic(ta.text);
+            else
+                Debug.Log("not found");
+        }
+    }
+    public string GetText(string key)
+    {
+        string value = "";
+        bool flag = _dic.TryGetValue(key, out value);
+        if (!flag)
+        {
+            value = key;
+         //   Debug.LogWarning(string.Format("the lauguage key {0} is not exist", key));
+            _dic.Add(key, value);
+        }
+        if (value == " ")
+            value = key;
+        return value;
+    }
+    public void SetBeizu(string key, string value)
+    {
+        if (_dic.ContainsKey(key))
+            _dic[key] = value;
+    }
+    void writeToDic(string str)
+    {
+        TextData data = XMLHelp.XmlHelper.XmlDeserialize<TextData>(str,System.Text.Encoding.UTF8);
+        if (data == null || data.data.Count == 0 || data.data[0].values.Count < (int)_type + 1)
+        {
+            Debug.LogError("fileConfig is not correct");
+            return;
+        }
+        _dic.Clear();
+        data.data.ForEach((x) =>
+        {
+            _dic.Add(x.key, x.values[(int)_type]);
+        });
+    }
+}
+[XmlRoot("TextData")]
+public class TextData
+{
+    public List<TextItem> data;
+
+    public TextData()
+    {
+        data = new List<TextItem>();
+    }
+    public void AddTextItem(TextItem item)
+    {
+        if (data.Exists((x) => x.key == item.key))
+        {
+            Debug.LogError(string.Format("textItem key = {0} is repeated", item.key));
+        }
+        else
+            data.Add(item);
+    }
+}
+public class TextItem
+{
+    [XmlElement("Key")]
+    public string key;
+    
+    public List<string> values;
+
+    public TextItem()
+    {
+        key = "";
+        values = new List<string>();
+    }
+    public TextItem(string key, string[] value)
+    {
+        this.key = key;
+        this.values = new List<string>();
+        for (int i = 0; i < value.Length; i++)
+        {
+            values.Add(value[i]);
+        }
+    }
+}
+
---
+++ Assets/Game/Scripts/Lauguage/LauguageMgr.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ILauguage
+{
+    /// <summary>
+    /// UI界面text设置
+    /// </summary>
+    void InitUIText();
+}
+
+public enum LauguageType
+{
+    China,
+    English,
+    Korea,
+    Jopan,
+
+
+    NULL,
+}
+
+/// <summary>
+/// 多语言管理类
+/// 支持动态切换语言
+/// </summary>
+class LauguageMgr
+{
+#region  //内部变量
+    //多语言文件路径 （放在streamingAsset目录,方便客户端动态更新）
+    public const string lauguageConfigFilePath = "";
+    private List<ILauguage> lauList;
+    private LauguageData data;
+    private static LauguageMgr _ins;
+    public static LauguageMgr GetIns() { if (_ins == null) _ins = new LauguageMgr(); return _ins; }
+    private LauguageMgr()
+    {
+        lauList = new List<ILauguage>();
+        data = new LauguageData(lauguageConfigFilePath);
+    }
+    public LauguageType curLauguage
+    {
+        get { return data.curLauguage; }
+    }
+    #endregion
+#region //外部接口
+    public void SetLauguage(LauguageType type)
+    {
+        data.SetLauguageType(type);
+    }
+    /// <summary>
+    /// 通过索引得到对应的值
+    /// </summary>
+    /// <param name="key">索引键</param>
+    /// <param name="beizu">备注（方便编辑 维护）</param>
+    /// <returns></returns>
+    public string GetText(string key,string beizu = "")
+    {
+        string value = data.GetText(key);
+#if UNITY_EDITOR 
+        if (string.Empty == value)
+            data.SetBeizu(key, beizu);
+#endif
+        return value;
+    }
+    public void ChangeLauguage(LauguageType type)
+    {
+        if (type == curLauguage) return;
+        SetLauguage(type);
+        lauList.ForEach((x) => {
+            x.InitUIText();
+        });
+    }
+    //public void SaveToFile()
+    //{
+    //    data.DeSerialize();
+    //}
+    public void AddILauguage(ILauguage lau)
+    {
+        lauList.Add(lau);
+    }
+    public void RemoveILauguage(ILauguage lau)
+    {
+        lauList.Remove(lau);
+    }
+#endregion
+}
