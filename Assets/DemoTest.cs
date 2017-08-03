f33099882ecbe77e78c13ab9800071efd1a278d0
ULynn <xiongsonglin_love@163.com>
2017-08-03 18:26:17 +0800

    res

---
+++ Assets/DemoTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFramework;
+public class DemoTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        AppFacade.Ins.GetMgr<ResourceManager>().LoadAsset("Cube", (obj) => {
+            //
+        });
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Game/Scripts/Core/ResourceMgr/AssetBundleInfo.cs
+++ Assets/Game/Scripts/Core/ResourceMgr/AssetBundleInfo.cs
@@ -11,7 +11,7 @@ namespace SimpleFramework
         }
         public static string assetPath_local       //本地资源地址
         {
-            get { return Application.streamingAssetsPath + "/Assetbundls/"+curPlatformFloder; }
+            get { return Application.streamingAssetsPath + "/Assetbundles/"+curPlatformFloder; }
         }
         public static string manifest_name         //AssetbundleManifest name
         {
